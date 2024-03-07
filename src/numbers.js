5eea7b133eb7f677fbc72e92a337b39170b303a7
Aman Ahmad <Rashid-Faraz@live.nmit.ac.nz>
2024-03-07 13:52:08 +1300

    my first test

---
+++ src/numbers.js
@@ -0,0 +1,12 @@
+//NOTE - i declared Const(keyword) num1(variable) is equal to Number 10
+const num1 = 10
+console.log (num1)
+//NOTE - the keyword let allow to given variable to change later on, num2 is the variable being assigned the value of 20
+let num2 = 20
+console.log (num2)
+//NOTE - 
+var num3= 30
+console.log (num3) 
+
+console.log (num1+num2)
+console.log (num3*num2)
