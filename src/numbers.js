895f71bd7d6e1a10f06b467809f908834aba0025
Aman Ahmad <Rashid-Faraz@live.nmit.ac.nz>
2024-03-07 14:59:34 +1300

    my first coading

--- src/numbers.js
+++ src/numbers.js
@@ -1,12 +1,43 @@
 //NOTE - i declared Const(keyword) num1(variable) is equal to Number 10
-const num1 = 10
-console.log (num1)
+const pi = 3.14
+// pi=10
+console.log (pi)
 //NOTE - the keyword let allow to given variable to change later on, num2 is the variable being assigned the value of 20
 let num2 = 20
+num2 = 25
+num2=35
 console.log (num2)
 //NOTE - 
 var num3= 30
 console.log (num3) 
-
-console.log (num1+num2)
+console.log (pi+num2)
 console.log (num3*num2)
+
+const fullName = 'Rashid Ahmad Faraz'
+console.log(fullName)
+
+const workPlace = 'NMIT'
+console.log(workPlace)
+console.log(fullName + " " + workPlace)
+console.log(`My full name is ${fullName} and I work at ${workPlace}`)
+const working = true
+const notWorking = false
+
+if (working) {
+    console.log(fullName)
+} 
+if(pi == 3.14){
+    console.log(fullName)
+}
+
+let a = 2
+let result 
+//if () {} else {}
+a=0
+if (a >= 0){
+    result = "Positive";
+    console.log(result)
+} else {
+    result = "NOT positive";
+    console.log(result)
+}
